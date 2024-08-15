9ca9f4995cc8179857b9d3f07b462f405d3db799
yongseok <dydtjrl08@naver.com>
2024-08-15 10:32:56 +0000

    240815 2d(2)

--- prac_root/root_hit.c
+++ prac_root/root_hit.c
@@ -21,17 +21,15 @@ void root_hit(){
 	tree -> SetBranchAddress("adc2",&adc2);
 
 	TCanvas *c1 = new TCanvas("test","2d",1200,800);	
-
+	c1 -> Divide(4,4);
 	
+	for(int i = 1; i < 17 ; i++){		
 		
-	TH2D *hist = new TH2D("hist","total",1000,0,8000,1000,0,3200);
-	for(int i = 0; i < tree -> GetEntries(); i++){	
-		tree -> GetEntry(i);
-		hist -> Fill(adc1,adc2);
-
+		TString namehist = Form("det1 == 2 && dch1 == %d",i);
+		TH2D *hist = new TH2D(namehist,Form("dch1 == %d",i),1000,0,8000,1000,0,3200);
+		tree -> Draw("adc2:adc1 >> "+namehist,namehist,"SAME ");
+		c1->Update();
+		c1 -> SaveAs(Form("det1_2_dch1_%d.jpg",i));
 	}
-	c1 -> cd();
-	hist -> Draw();
-	c1 -> SaveAs("hist2d.jpg");
 
 }
